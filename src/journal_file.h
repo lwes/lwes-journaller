5ebc1a1486ee5935572dd52109df36942f60f430
Frank Maritato <frankmarit@gmail.com>
2009-04-20 23:06:57 +0000

    forgot header
    git-svn-id: https://lwes.svn.sourceforge.net/svnroot/lwes/lwes-journaller/trunk@100 a2f82657-cdd2-4550-bd36-68a8e7111808

--- src/journal_file.h
+++ src/journal_file.h
@@ -1,5 +1,22 @@
-/* $Id: journal_file.h,v 1.1.1.1 2004/05/20 21:21:03 hightowg Exp $
- */
+/*======================================================================*
+ * Copyright (C) 2008 Light Weight Event System                         *
+ * All rights reserved.                                                 *
+ *                                                                      *
+ * This program is free software; you can redistribute it and/or modify *
+ * it under the terms of the GNU General Public License as published by *
+ * the Free Software Foundation; either version 2 of the License, or    *
+ * (at your option) any later version.                                  *
+ *                                                                      *
+ * This program is distributed in the hope that it will be useful,      *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of       *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        *
+ * GNU General Public License for more details.                         *
+ *                                                                      *
+ * You should have received a copy of the GNU General Public License    *
+ * along with this program; if not, write to the Free Software          *
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor,                   *
+ * Boston, MA 02110-1301 USA.                                           *
+ *======================================================================*/
 
 #ifndef JOURNAL_FILE_DOT_H
 #define JOURNAL_FILE_DOT_H
--- src/marshal.h
+++ src/marshal.h
@@ -1,5 +1,22 @@
-/* $Id: marshal.h,v 1.3 2006/11/29 13:36:56 woodg Exp $
- */
+/*======================================================================*
+ * Copyright (C) 2008 Light Weight Event System                         *
+ * All rights reserved.                                                 *
+ *                                                                      *
+ * This program is free software; you can redistribute it and/or modify *
+ * it under the terms of the GNU General Public License as published by *
+ * the Free Software Foundation; either version 2 of the License, or    *
+ * (at your option) any later version.                                  *
+ *                                                                      *
+ * This program is distributed in the hope that it will be useful,      *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of       *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        *
+ * GNU General Public License for more details.                         *
+ *                                                                      *
+ * You should have received a copy of the GNU General Public License    *
+ * along with this program; if not, write to the Free Software          *
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor,                   *
+ * Boston, MA 02110-1301 USA.                                           *
+ *======================================================================*/
 
 #ifndef MARSHAL_DOT_H
 #define MARSHAL_DOT_H
