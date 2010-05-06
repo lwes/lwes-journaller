fa66779661fef935d340fc472daa3f6f9d0503b9
Preston Pfarner <preston.pfarner@gmail.com>
2010-05-06 08:28:35 +0000

    Use blocking reads unless we're shutting down, so we don't have to call time() once [more] per event.
    git-svn-id: https://lwes.svn.sourceforge.net/svnroot/lwes/lwes-journaller/trunk@475 a2f82657-cdd2-4550-bd36-68a8e7111808

--- src/queue_mqueue.c
+++ src/queue_mqueue.c
@@ -135,7 +135,6 @@ static int xread (struct queue* this_queue, void* buf,
   struct mq_attr attr;
   int mq_rec_rtrn;
   unsigned int pri;
-  struct timespec time_buf = { time(NULL)+1, 500000000 };
 
   if ( 0 == buf )
     {
@@ -150,7 +149,18 @@ static int xread (struct queue* this_queue, void* buf,
 
   LOG_PROG("about to call mq_receive().  gbl_done=%d\n", gbl_done);
 
-  if ( (mq_rec_rtrn = mq_timedreceive (ppriv->mq, buf, count, &pri, &time_buf)) < 0 )
+  /* use blocking reads unless we're shutting down. */
+  if (gbl_done)
+  {
+    struct timespec time_buf = { time(NULL)+1, 500000000 };
+    mq_rec_rtrn = mq_timedreceive (ppriv->mq, buf, count, &pri, &time_buf);
+  }
+  else
+  {
+    mq_rec_rtrn = mq_receive (ppriv->mq, buf, count, &pri);
+  }
+
+  if (mq_rec_rtrn  < 0 )
     {
       LOG_PROG("errno: %d %s\n", errno, strerror(errno));
       switch ( errno )
