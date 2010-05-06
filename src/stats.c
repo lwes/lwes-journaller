924edc0d30699b221c27930fb2bfaa9fcbaf9f12
Preston Pfarner <preston.pfarner@gmail.com>
2010-05-06 11:21:18 +0000

    Add a one-line stats summary, designed for easy aggregation.
    git-svn-id: https://lwes.svn.sourceforge.net/svnroot/lwes/lwes-journaller/trunk@476 a2f82657-cdd2-4550-bd36-68a8e7111808

--- src/stats.c
+++ src/stats.c
@@ -255,4 +255,7 @@ void stats_report(struct stats* st)
     {
       LOG_INF(" %g bps, %g pps.\n", bps, pps);
     }
+
+  LOG_INF("Stats summary:\t%d\t%lld\t%lld\t%g\t%g\t%s",
+          now, st->bytes_total, st->packets_total, bps, pps);
 }
