abbfec0b3afab6d98561d7923817a4b7394db71c
Anthony Molinaro <anthonym@alumni.caltech.edu>
2015-11-16 14:46:11 -0800

    refactor to deal with exact seconds

--- src/time_utils.c
+++ src/time_utils.c
@@ -20,16 +20,26 @@ time_to_next_round_interval (const struct timeval now,
    * the interval which gives us the next interval start */
   time_t next_interval = second_in_day / interval * interval + interval;
 
-  /* next we'll subtract to get seconds to next interval (also subtracting
-   * one as we'll also be checking millis below */
-  time_t seconds_to_next_interval = next_interval - second_in_day - 1;
-
   /* do a similar calculation to determine approximate micros until next round
    * interval */
   unsigned long long now_millis = (((long long)now.tv_sec) * 1000LL)
                                    + (long long)(now.tv_usec/1000LL);
-  unsigned long long micros_to_next_second =
-    (((now_millis / 1000LL) * 1000LL + 1000LL) - now_millis) * 1000LL;
+
+  /* next we'll subtract to get seconds to next interval */
+  time_t seconds_to_next_interval = next_interval - second_in_day;
+
+  /* calculate micros (but in milliseconds) */
+  unsigned long long micros_to_next_second = 0;
+
+  /* if we hit an exact second there are no millis to wait, but if we did
+   * not we need to calculate the millis until the next second, and subtract
+   * a second as the calculation above assumed round seconds
+   */
+  if (now_millis != 0) {
+    micros_to_next_second =
+      (((now_millis / 1000LL) * 1000LL + 1000LL) - now_millis) * 1000LL;
+    seconds_to_next_interval -= 1;
+  }
 
   next->tv_sec = seconds_to_next_interval;
   next->tv_usec = micros_to_next_second;
