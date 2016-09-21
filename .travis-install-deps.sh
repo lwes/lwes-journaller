a93aa2f7900f1b4bed7b90a6980009f48518fcd5
Anthony Molinaro <anthonym@alumni.caltech.edu>
2016-09-21 23:40:07 +0000

    standardize my use of travis a bit between lwes/mondemand things

---
+++ .travis-install-deps.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+set -e
+cwd=`pwd`
+depdir="$cwd/deps"
+mkdir -p $depdir
+srcdir="$depdir/source"
+mkdir -p $srcdir
+export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$depdir/lib/pkgconfig
+
+LWES_VERSION=0.25.1
+MONDEMAND_VERSION=4.3.1
+
+# install lwes
+cd $srcdir
+wget https://github.com/lwes/lwes/releases/download/${LWES_VERSION}/lwes-${LWES_VERSION}.tar.gz
+tar -xzvf lwes-${LWES_VERSION}.tar.gz
+cd lwes-${LWES_VERSION} \
+  && ./configure --disable-hardcore --prefix=$depdir \
+  && make install
+
+# install mondemand
+cd $srcdir;
+wget https://github.com/mondemand/mondemand/releases/download/${MONDEMAND_VERSION}/mondemand-${MONDEMAND_VERSION}.tar.gz
+tar -xzvf mondemand-${MONDEMAND_VERSION}.tar.gz;
+cd mondemand-${MONDEMAND_VERSION} \
+  && ./configure --disable-hardcore --prefix=$depdir \
+  && make install
